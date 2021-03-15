beef48d476b7a0de28bc0a05b1e50aaa61a9e7c0
Sen SEAN <sen.sean2@gmail.com>
2021-03-15 08:31:34 +0700

    udpated UI when new iPhone's theme (dark mode)

---
+++ JomhamsTest/Common/Extension/UILabel.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  JomhamsTest
+//
+//  Created by MCNC on 10/3/2564 BE.
+//  Copyright © 2564 BE SEAN Sen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UILabel {
+    func labelColor() {
+        self.textColor = UIColor.black
+    }
+}
---
+++ JomhamsTest/Common/Extension/UITextLabel.swift
@@ -0,0 +1,17 @@
+//
+//  UITextLabel.swift
+//  JomhamsTest
+//
+//  Created by MCNC on 8/2/2564 BE.
+//  Copyright © 2564 BE SEAN Sen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UITextView {
+    func textBackground(color: UIColor) {
+        self.backgroundColor = color
+    }
+    
+}
